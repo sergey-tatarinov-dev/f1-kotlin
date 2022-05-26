2f4251e7f7fad288a99020de082974d75b6a395a
Sergey Tatarinov <serhii.tatarinov@gmail.com>
2022-05-26 13:52:30 +0300

    Convert concatenation to raw string

--- src/main/kotlin/ru/project/f1/repository/GrandPrixResultRepository.kt
+++ src/main/kotlin/ru/project/f1/repository/GrandPrixResultRepository.kt
@@ -11,26 +11,34 @@ import java.math.BigInteger
 @Repository
 interface GrandPrixResultRepository : JpaRepository<GrandPrixResult, BigInteger> {
 
-    @Query("select distinct grandPrix.id from GrandPrixResult order by grandPrix.id")
+    @Query("""
+        select distinct grandPrix.id 
+        from GrandPrixResult 
+        order by grandPrix.id
+    """)
     fun findDistinctByGrandPrixId(): List<BigInteger>
 
-    @Query("select NEW ru.project.f1.entity.GrandPrixResultPerGrandPrix(" +
-            "d.id, gpr.position, concat(d.name, ' ', d.surname), d.country.id, t.name, t.file.id" +
-            ") " +
-            "from GrandPrixResult gpr " +
-            "   inner join Driver d ON gpr.driver.id = d.id " +
-            "   inner join GrandPrix gp on gpr.grandPrix.id = gp.id " +
-            "   inner join LineUp lu on lu.lineUpId.driverId = d.id " +
-            "   inner join Team t on t.id = lu.lineUpId.teamId " +
-            "where gpr.grandPrixResultId.grandPrixId = :id " +
-            "order by gpr.position")
+    @Query("""
+        select NEW ru.project.f1.entity.GrandPrixResultPerGrandPrix(
+        d.id, gpr.position, concat(d.name, ' ', d.surname), d.country.id, t.name, t.file.id
+        )
+        from GrandPrixResult gpr
+            inner join Driver d ON gpr.driver.id = d.id
+            inner join GrandPrix gp on gpr.grandPrix.id = gp.id
+            inner join LineUp lu on lu.lineUpId.driverId = d.id
+            inner join Team t on t.id = lu.lineUpId.teamId 
+        where gpr.grandPrixResultId.grandPrixId = :id
+        order by gpr.position
+    """)
     fun findAllByGrandPrixId(id: Int): List<GrandPrixResultPerGrandPrix>
 
-    @Query("select NEW ru.project.f1.entity.GrandPrixResultPerDriver(gp.id, gp.fullName, gpr.points) " +
-            "from GrandPrixResult gpr " +
-            "inner join GrandPrix gp on gp.id = gpr.grandPrixResultId.grandPrixId " +
-            "where gpr.grandPrixResultId.driverId = :id " +
-            "order by gpr.grandPrixResultId.driverId")
+    @Query("""
+        select NEW ru.project.f1.entity.GrandPrixResultPerDriver(gp.id, gp.fullName, gpr.points) 
+        from GrandPrixResult gpr
+        inner join GrandPrix gp on gp.id = gpr.grandPrixResultId.grandPrixId 
+        where gpr.grandPrixResultId.driverId = :id
+        order by gpr.grandPrixResultId.driverId
+    """)
     fun findAllByDriverId(id: Int): List<GrandPrixResultPerDriver>
 
 }
