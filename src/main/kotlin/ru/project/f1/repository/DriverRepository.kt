3f4d92545eb6de7315341cb9ab9cf24b56031188
Sergey Tatarinov <serhii.tatarinov@gmail.com>
2022-12-07 18:28:43 +0300

    Add select of years for drivers list

--- src/main/kotlin/ru/project/f1/repository/DriverRepository.kt
+++ src/main/kotlin/ru/project/f1/repository/DriverRepository.kt
@@ -1,9 +1,32 @@
 package ru.project.f1.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import ru.project.f1.entity.Driver
 import java.math.BigInteger
 
 @Repository
-interface DriverRepository : JpaRepository<Driver, BigInteger>
+interface DriverRepository : JpaRepository<Driver, BigInteger> {
+    @Query("""
+        select distinct extract(year from date) as year 
+        from grand_prix 
+        order by year
+    """, nativeQuery = true)
+    fun findAllGrandPrixYears(): List<Int>
+
+    @Query("""
+        select NEW ru.project.f1.entity.Driver(d.id, d.name, d.surname, d.raceNumber, d.country)
+        from LineUp l inner join Driver d ON l.lineUpId.driverId = d.id 
+        where l.year = :year
+        order by l.lineUpId.teamId
+    """)
+    fun findAllDriversByYear(year: Int): List<Driver>
+
+    @Query("""
+        select distinct year 
+        from LineUp
+        order by year
+    """)
+    fun findAllDriverYears(): List<Int>
+}
